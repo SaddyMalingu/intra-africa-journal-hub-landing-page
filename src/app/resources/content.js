ad5443e115174c3381985a56a30c8e436e6e5f4c
SaddyMalingu <127030808+SaddyMalingu@users.noreply.github.com>
2025-05-27 22:23:20 -0700

    Update content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -9,7 +9,7 @@ const person = {
   role: "Design Engineer",
   avatar: "/images/avatar.jpg",
   email: "example@gmail.com",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  location: "Africa/Nairobi", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
 };
 
